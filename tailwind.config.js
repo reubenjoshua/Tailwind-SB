1a6f24dae35f8456732e3792e46052de06f58e3e
aaairam <magildaceria@gmail.com>
2023-08-03 15:46:04 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'gurey':'#2C2C32',
+        'snow' : '#CDC9C9'
+      }
+    },
+  },
+  plugins: [],
+}
+
