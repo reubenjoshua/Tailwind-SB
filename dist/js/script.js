860e7d566db9a5ac30ef700d2bf89eb23b52ab56
aaairam <magildaceria@gmail.com>
2023-08-04 09:16:01 +0800

    headerdone

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+const headerNav = document.querySelector(".header__nav");
+const toggleMenu = document.querySelector(".toggle__menu");
+toggleMenu.addEventListener("click", () => {
+    toggleMenu.classList.toggle("open");
+    headerNav.classList.toggle("open");
+
+});
